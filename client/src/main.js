efbf3cbd72eb5fd22bf534d0a83a11e2e2a433f9
costpawa <hakan_dnz_000@hotmail.com>
2021-10-08 09:19:52 +0300

    Add 'can' and 'is' directives

--- client/src/main.js
+++ client/src/main.js
@@ -9,6 +9,33 @@ import './assets/css/index.css'
 
 Vue.config.productionTip = false
 
+Vue.directive('can', (el, binding, vnode) => {
+  let userPermissions = localStorage.getItem(process.env.VUE_APP_STORAGE_NAME + '_user_permissions')
+  if(typeof(userPermissions) === 'undefined' || userPermissions === null || userPermissions === 'null' || userPermissions === '') {
+    userPermissions = sessionStorage.getItem(process.env.VUE_APP_STORAGE_NAME + '_user_permissions')
+  }
+
+  if(userPermissions.split(',').indexOf(binding.value) !== -1) {
+     return vnode.elm.hidden = false;
+  } else {
+     return vnode.elm.hidden = true;
+  }
+})
+
+Vue.directive('is', (el, binding, vnode) => {
+  let userRoles = localStorage.getItem(process.env.VUE_APP_STORAGE_NAME + '_user_roles')
+  if(typeof(userRoles) === 'undefined' || userRoles === null || userRoles === 'null' || userRoles === '') {
+    userRoles = sessionStorage.getItem(process.env.VUE_APP_STORAGE_NAME + '_user_roles')
+  }
+
+  if(userRoles.split(',').indexOf(binding.value) !== -1) {
+     return vnode.elm.hidden = false;
+  } else {
+     return vnode.elm.hidden = true;
+  }
+})
+
+
 new Vue({
   router,
   store,
