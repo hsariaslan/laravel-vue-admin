6d03cb236d087029e558c45c64ecc90f061b6933
costpawa <hakan_dnz_000@hotmail.com>
2021-10-08 20:29:00 +0300

    Create apiCrypt helper function

--- server/app/Http/helpers.php
+++ server/app/Http/helpers.php
@@ -2,15 +2,25 @@
 if (!function_exists('slugify')) {
     function slugify ($text, string $divider = '_')
     {
-    $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
-    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-    $text = preg_replace('~[^-\w]+~', '', $text);
-    $text = trim($text, $divider);
-    $text = preg_replace('~-+~', $divider, $text);
-    $text = strtolower($text);
-    if (empty($text)) {
-        return 'n-a';
+        $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        $text = preg_replace('~[^-\w]+~', '', $text);
+        $text = trim($text, $divider);
+        $text = preg_replace('~-+~', $divider, $text);
+        $text = strtolower($text);
+        if (empty($text)) {
+            return 'n-a';
+        }
+        return $text;
     }
-    return $text;
+}
+
+if (!function_exists('apiCrypt')) {
+    function apiCrypt ($data)
+    {
+        $cipher = 'AES-128-ECB';
+        $key = env('API_CRYPT_KEY');
+        $encoded = openssl_encrypt($data, $cipher, $key);
+        return $encoded;
     }
 }
