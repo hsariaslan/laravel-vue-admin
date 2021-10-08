c5dca969048de3d0e9a28a60efbe896afea6f0b5
costpawa <hakan_dnz_000@hotmail.com>
2021-10-08 20:29:42 +0300

    Minor fixes

--- server/app/Http/Controllers/Api/v1/AuthController.php
+++ server/app/Http/Controllers/Api/v1/AuthController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use Illuminate\Http\Request;
 use App\Http\Resources\ProfileResource;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login (LoginRequest $request):ProfileResource
+    public function login (LoginRequest $request)
     {
         if (Auth::attempt($request->validated())) {
             $request->session()->regenerate();
@@ -23,7 +22,7 @@ class AuthController extends Controller
         ], 401);
     }
 
-    public function logout(Request $request)
+    public function logout()
     {
         Auth::logout();
 
